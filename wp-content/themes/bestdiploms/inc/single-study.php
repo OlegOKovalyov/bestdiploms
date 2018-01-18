33a978e33e87e44cf93a8fd927d86f68bdf3789f
Oleg Kovalyov <koa2003@ukr.net>
2018-01-18 12:34:55 +0200

    Добавил вывод на сайт CPT study(Документы) с помощью single-study.php и content-study.php.

--- wp-content/themes/bestdiploms/inc/single-study.php
+++ wp-content/themes/bestdiploms/inc/single-study.php
@@ -1,49 +1,33 @@
 <?php
  /**
-  * Template Name: New Template Study
-  * Template Post Type: study
+  * The template for displaying all CPT study posts
+  *
+  * @package Best_Diploms  
   */
 
 get_header(); ?>
 
-<div id="primary">
-    <div id="content" role="main">
-    <?php
-    $mypost = array( 'post_type' => 'study', );
-    $loop = new WP_Query( $mypost );
-    ?>
-    <?php while ( $loop->have_posts() ) : $loop->the_post();?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <!-- Display featured image in right-aligned floating div -->
-                <div style="float: right; margin: 10px">
-                    <?php the_post_thumbnail( array( 100, 100 ) ); ?>
-                </div>
-                <!-- Display Title and Author Name -->
-                <strong>Title: </strong><?php the_title(); ?><br />
-                <strong>Director: </strong>
-                <?php echo esc_html( get_post_meta( get_the_ID(), 'movie_director', true ) ); ?>
-                <br />
-                <!-- Display yellow stars based on rating -->
-                <strong>Rating: </strong>
-                <?php
-                $nb_stars = intval( get_post_meta( get_the_ID(), 'movie_rating', true ) );
-                for ( $star_counter = 1; $star_counter <= 5; $star_counter++ ) {
-                    if ( $star_counter <= $nb_stars ) {
-                        echo '<img src="' . plugins_url( 'Movie-Reviews/images/icon.png' ) . '" />';
-                    } else {
-                        echo '<img src="' . plugins_url( 'Movie-Reviews/images/grey.png' ). '" />';
-                    }
-                }
-                ?>
-            </header>
-            <!-- Display movie review contents -->
-            <div class="entry-content"><?php the_content(); ?></div>
-        </article>
-    <?php endwhile; ?>
-    </div>
-</div>
+    <div id="primary" class="content-area col-lg-9 order-2">
+        <main id="main" class="site-main">
 
-<?php wp_reset_query(); ?>
+            <?php
+            while ( have_posts() ) : the_post();
 
-<?php get_footer(); ?>
+                get_template_part( 'template-parts/content', get_post_type() );
+
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+
+            endwhile; // End of the loop.
+            ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+    <div class="col-lg-3 col-sidebar-primary">
+        <?php get_sidebar(); ?>
+    </div>          
+  
+<?php get_footer(); 
---
+++ wp-content/themes/bestdiploms/template-parts/content-study-0w.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * Template part for displaying CPT study content in single.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Best_Diploms
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php /*the_title( '<h1 class="entry-title">', '</h1>' );*/ ?>
+	</header><!-- .entry-header -->
+
+		<!--<p class="site-title"><a href="<?php /*echo esc_url( home_url( '/' ) );*/ ?>" rel="home"><?php /*bloginfo( 'name' );*/ ?></a></p>-->
+
+
+
+		<!-- Вывод эталажей (CPT 'etalage') на странице Цены -->
+		<div class="row">
+			<div class="entry-content col-lg-9">
+				<?php if ( function_exists( 'dimox_breadcrumbs' ) ) dimox_breadcrumbs(); ?>
+				<h1><?php the_title() ?></h1>
+				<!-- Display CPT study content -->
+				<?php the_post_thumbnail(/* array( 100, 100 ) */); ?>
+            	<div class="entry-content"><?php the_content(); ?></div>
+
+
+    <?php
+    $mypost = array( 'post_type' => 'study', );
+    $loop = new WP_Query( $mypost );
+    ?>
+    <?php while ( $loop->have_posts() ) : $loop->the_post();?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <!-- Display featured image in right-aligned floating div -->
+                <div style="float: right; margin: 10px">
+                    <?php the_post_thumbnail( array( 100, 100 ) ); ?>
+                </div>
+
+                <!-- Display Title and Author Name -->
+                <strong>Title: </strong><?php the_title(); ?><br />
+                <strong>Director: </strong>
+                <?php echo esc_html( get_post_meta( get_the_ID(), 'movie_director', true ) ); ?>
+                <br />
+                
+                <!-- Display yellow stars based on rating -->
+                <strong>Rating: </strong>
+                <?php
+                $nb_stars = intval( get_post_meta( get_the_ID(), 'movie_rating', true ) );
+                for ( $star_counter = 1; $star_counter <= 5; $star_counter++ ) {
+                    if ( $star_counter <= $nb_stars ) {
+                        echo '<img src="' . plugins_url( 'Movie-Reviews/images/icon.png' ) . '" />';
+                    } else {
+                        echo '<img src="' . plugins_url( 'Movie-Reviews/images/grey.png' ). '" />';
+                    }
+                }
+                ?>
+            </header>
+            <!-- Display movie review contents -->
+            <div class="entry-content"><?php the_content(); ?></div>
+        </article>
+    <?php endwhile; ?>
+
+
+
+
+
+
+				    <!-- Displaying the Custom Post 'etalage' on Price Page (can display anywhere). --> 
+				    <?php $args = array(
+			        	'order'			 => 'DESC',
+			        	'post_parent'	 => 173, // ID эталажа Образование (дочерняя запись к записи Документы)
+			        	'orderby'		 => 'parent',
+			            'post_type' 	 => 'etalage',
+			            'posts_per_page' => -1,
+			            'category_name'  => 'education-all', // Все виды образования
+			            //'category_name' => 'education1',
+			            //'category_name' => 'education2',
+			            //'category_name' => 'documents-pop', // Самые популярные документы
+			        );  
+			        $extra_loop = new WP_Query( $args ); /*print_r ($extra_loop);*/
+			        if ( $extra_loop->have_posts() ) : while ( $extra_loop->have_posts() ) : $extra_loop->the_post();
+
+				        $post_id = $post->ID; // Current CPT
+				        $wp_post_parent_id = wp_get_post_parent_id( $post_id ); // Parent CPT ?>
+
+						<div class="price-item">
+
+					    	<?php 
+				    		//$wp_post_parent_cat_id = wp_get_post_parent_id( $post->ID );
+				    		//$post_cat_id = $post->ID;
+				            while(  $post->ID  == $post_id ) { ?>
+				                <h3 class="doc-item-title"><?php the_title(); ?></h3>
+						
+								<?php $in_args = array(
+						        	'order'			 => 'ASC',
+						        	'post_parent'	 => $post_id,
+						        	//'orderby'		 => 'parent',
+						            'post_type' 	 => 'etalage',
+						            'posts_per_page' => -1,
+						            'category_name'  => 'education-all', // Все виды образования
+						            //'category_name' => 'education1',
+						            //'category_name' => 'education2',
+						            //'category_name' => 'documents-pop', // Самые популярные документы
+							        ); 		
+
+								$in_loop = new WP_Query( $in_args );
+				        		if ( $in_loop->have_posts() ) : while ( $in_loop->have_posts() ) : $in_loop->the_post(); 		
+				        		//$meta = get_post_meta( $post->ID, 'your_fields', true ); 
+								$value1 = get_post_field( "make" ); // Meta-box for Document Creation Date
+								$value2 = get_post_field( "gznk-price" ); // Meta-box for GOZNAK Price
+								$value = get_post_field( "price" );	// Meta-box for Typography Price
+						            if( $value || $value2 ) { ?>
+
+										<section>
+									        <a href="#" target="_blank" rel="noopener">
+									            <?php if ( has_post_thumbnail() ) {
+									                the_post_thumbnail();
+									            } ?>
+									        </a>
+										    <div class="price-props">
+										        <h4 class="price-item-title"><?php the_title(); ?> <span class="price-item-year"> <?php echo $value1; ?></span></h4>
+
+										        <?php					            
+									                echo '<p class="page-price-gznk">' . $value2 . '</p>';
+									                echo '<p class="page-price-tpgrf">' . $value . '</p>';
+								        		?>
+
+								                <a class="btn btn-danger" href="#" role="button">Заказать</a>
+
+									<?php  } else {
+										/* echo '<p>empty</p>';*/
+									} ?>
+
+									    </div><!-- .price-props -->
+							    	</section>
+				        
+							<?php endwhile; 
+							endif; } ?>
+
+						</div><!-- .price-item -->
+
+					<?php endwhile; endif; wp_reset_postdata(); ?>  
+		
+					<?php the_content();
+				
+					/*wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bestdiploms' ),
+						'after'  => '</div>',
+					) );*/ 
+
+					?>
+
+			</div><!-- .entry-content -->
+
+			<div class="col-lg-3 sb-secondry">
+		        <?php get_sidebar( 'right' ); ?>
+		    </div>
+	    </div><!-- .row -->  
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Edit <span class="screen-reader-text">%s</span>', 'bestdiploms' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
